9dd7c811ee477ae2461618996a5708c54f675a89
Tadeu <tadeunascimento36@gmail.com>
2021-01-08 15:35:30 -0300

    Commit da versão incial do projeto portal de classificados.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { BannerComponent } from './banner/banner.component';
+import { SectionComponent } from './section/section.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { BannerBottomComponent } from './banner-bottom/banner-bottom.component';
+import { FooterComponent } from './footer/footer.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    BannerComponent,
+    SectionComponent,
+    CarouselComponent,
+    BannerBottomComponent,
+    FooterComponent  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
